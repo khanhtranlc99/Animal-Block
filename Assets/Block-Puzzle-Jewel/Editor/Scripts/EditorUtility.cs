6e47a355acc1aa034b4eb25aa90f922b87ca50a0
trankhanhlc <trankhanhlc99@gmail.com>
2022-06-08 22:51:18 +0700

    s

---
+++ Assets/Block-Puzzle-Jewel/Editor/Scripts/EditorUtility.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+using System.IO;
+
+public class EditorUtility : MonoBehaviour {
+
+    [MenuItem("Bimbimnet/Plugin Setup/Check Setup", false, 1)]
+    private static void SetUp()
+    {
+        DependencyChecker.OpenWelcomeWindow();
+    }
+
+    //[MenuItem("Block Magic Puzzle/Plugin Setup/Setup IAP Catalog",false,2)]
+    //private static void SetUpIAPCatalog()
+    //{
+    //	string sourcePath = Application.dataPath +"/Block-Puzzle-Magic/UnityIAPCatalog/IAPProductCatalog.json";
+    //	string destPath = Application.dataPath +"/Plugins/UnityPurchasing/Resources/IAPProductCatalog.json";
+
+    //	if(File.Exists(sourcePath))
+    //	{
+    //		if(!File.Exists(destPath))
+    //		{		
+    //			File.Copy(sourcePath, destPath);
+    //			AssetDatabase.Refresh();
+    //		}
+    //		else
+    //		{
+    //			Debug.LogWarning("IAP Catalog already exists.");
+    //		}
+    //	}
+    //}
+
+    [MenuItem("Bimbimnet/Clear PlayerPrefs")]
+	private static void ClearPrefs()
+	{
+		PlayerPrefs.DeleteAll();
+	}
+
+	[MenuItem("Bimbimnet/Persistent Data Path/Remove All Files")]
+	private static void DeleteAllFilesFromPersistentDataPath()
+	{
+		FileUtil.DeleteFileOrDirectory (Application.persistentDataPath);
+	}
+
+	
+
+}
